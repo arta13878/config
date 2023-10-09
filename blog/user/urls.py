9ca5d7c451b8eb03d44753b4c1cc86bf6f15eace
konaniarta8@gmail.com <konaniarta8@gmail.com>
2023-10-09 12:51:21 +0330

    ddd

--- blog/user/urls.py
+++ blog/user/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path,include
-
+from .views import userCreateView
 
 urlpatterns = [
     path('',include('django.contrib.auth.urls')),
-    
+    path('signup',userCreateView.as_view())
 ]
 
--- blog/user/views.py
+++ blog/user/views.py
@@ -4,3 +4,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
    
+class userCreateView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
